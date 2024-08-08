9a313573d2756610ae460ca0dbcc31c9ffb50e38
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-08 15:20:46 -0300

    :sparkles: feat(tutorRouter): adicionar rota de busca por id

--- src/router/tutorRouter.js
+++ src/router/tutorRouter.js
@@ -3,7 +3,11 @@ const route = express();
 const tutorController = require("../controller/tutorController");
 
 
+// no primeiro parametro passo minhas rotas e no segundo passo meu meotodo
+//isso significa na implmentação do metodos o meu getAllController tem acesso ao oque esta vindo na minha request
 route.get('/tutors', tutorController.getAllTutor)
+
+route.get('/tutor/:id', tutorController.getById)
 
 route.post('/tutor', tutorController.createTutor)
 
