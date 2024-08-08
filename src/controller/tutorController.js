77dd9e08634f831deddce035803c7cf7637deffc
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-08 14:28:07 -0300

    :sparkles: feat(tutorController): adicionar metodo de busca por id

--- src/controller/tutorController.js
+++ src/controller/tutorController.js
@@ -1,12 +1,14 @@
 const tutor = require("../models/tutors");
 
-
+// Como meu controller e minha porta de entrada da API e por aqui que recebo as request e devolvo as responses
 class tutorClinic {
 
     createTutor = async (req,res) =>{
+
         let {name, phone,email,date_of_birth,zip_code} = req.body;
 
         const newTutor =  tutor.create({name,phone,email,date_of_birth,zip_code}).then(resul =>{
+            //devolvo a minha response em Json para meu Client com status Create
             res.status(201).json(resul);
         }).catch((err)=>{
             res.status(500).json(err);
@@ -15,8 +17,14 @@ class tutorClinic {
 
     }
 
-    getById(id) {
-        return 'tutor 1'
+    getById = async (req,res)=>{
+        const id = req.params.id;
+        tutor.findByPk(id).then(result=>{
+            res.status(200).json(result);
+        }).catch((err)=>{
+            console.log(err)
+        })
+
     }
 
     getAllTutor = async (req,res)=>{
