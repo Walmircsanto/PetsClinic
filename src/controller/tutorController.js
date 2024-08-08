339c4c4a4adf92d3537211e726ad7cf0bef472a3
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-08 17:30:20 -0300

    :sparkles: feat(tutorController): adicionar metodo de atualizar

--- src/controller/tutorController.js
+++ src/controller/tutorController.js
@@ -3,40 +3,49 @@ const tutor = require("../models/tutors");
 // Como meu controller e minha porta de entrada da API e por aqui que recebo as request e devolvo as responses
 class tutorClinic {
 
-    createTutor = async (req,res) =>{
+    createTutor = async (req, res) => {
 
-        let {name, phone,email,date_of_birth,zip_code} = req.body;
+        let {name, phone, email, date_of_birth, zip_code} = req.body;
 
-        const newTutor =  tutor.create({name,phone,email,date_of_birth,zip_code}).then(resul =>{
+        const newTutor = tutor.create({name, phone, email, date_of_birth, zip_code}).then(resul => {
             //devolvo a minha response em Json para meu Client com status Create
             res.status(201).json(resul);
-        }).catch((err)=>{
+        }).catch((err) => {
             res.status(500).json(err);
         });
 
 
     }
 
-    getById = async (req,res)=>{
+    getById = async (req, res) => {
         const id = req.params.id;
-        tutor.findByPk(id).then(result=>{
+        tutor.findByPk(id).then(result => {
             res.status(200).json(result);
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err)
         })
 
     }
 
-    getAllTutor = async (req,res)=>{
-        tutor.findAll().then(result=>{
-            res.status(200).json({result:result})
-        }).catch((err)=>{
+    getAllTutor = async (req, res) => {
+        tutor.findAll().then(result => {
+            res.status(200).json({result: result})
+        }).catch((err) => {
             console.log(err)
         })
     }
 
-    updateTutor(id) {
-        return 'tutor Update'
+    updateTutor = async (req, res) => {
+        const id = req.params.id;
+        console.log(id)
+        let {name, phone, email, date_of_birth, zip_code} = req.body;
+        await tutor.update(req.body, {
+            where: {id: id}
+        }).then(result => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(500).json(err);
+        })
     }
 
     deleteTutor(id) {
