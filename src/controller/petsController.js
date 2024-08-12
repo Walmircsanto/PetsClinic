10d62ddac086c192ff651187469257ed3e1af09d
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-12 13:08:51 -0300

    :sparkles: feat: Adicionar endpoints de criar pets

--- src/controller/petsController.js
+++ src/controller/petsController.js
@@ -1,9 +1,26 @@
 const pets = require('../models/pets')
+const tutor = require("../models/tutors");
 
 class PetsClinic {
 
-    createPets(){
-        console.log("Creating Pets");
+    createPet = async (req, res) => {
+
+        //pegar Id que esta na URL
+        const idTu = req.params.id
+        let {name, species, carry, weight, date_of_birth, idTutor=idTu} = req.body;
+
+        console.log(idTu)
+        //verifica se o tutor daquele ID existe
+        const tuto = await tutor.findByPk(idTutor)
+        if (tuto) {
+            pets.create({name, species, carry, weight, date_of_birth, idTutor}).then(result=>{
+                res.status(200).json(result)
+            }).catch((err)=>{
+                res.json(err)
+            })
+        }
+
+
     }
 
     getAllPets(){
