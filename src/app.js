f9a0dab496cf7fc4b88fad520a95f898c2719ba9
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-13 17:04:35 -0300

    :sparkles: feat: adicionar configurações do swagger

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const PORT = 3000;
 const tutorRouter = require('./router/tutorRouter');
 const petRouter = require('./router/petsRouter');
 const bodyParser = require('body-parser');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger.json');
 
 
 // Parse incoming requests with JSON payloads
@@ -20,15 +21,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(tutorRouter)
 app.use(petRouter)
-app.use(db.sync)
-app.use(db.authenticate)
+
+db.authenticate().then(()=>{
+    console.log('Database MySQL connected ')
+}).catch((err)=>{
+    console.log(` Database MySQL not connected ${err}`);
+});
+
+
+db.sync({force:true}).then(r =>{
+    console.log(`Database created ${r}`)
+}).catch((err)=>{
+    console.log(err)
+});
 
 
 
 // inicialização da aplicação
 app.listen(PORT,()=>{
     console.log(`Server started on port ${PORT}`);
+    console.log("API documentation: http://localhost:3000/docs");
 })
 
+app.use(bodyParser.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 
